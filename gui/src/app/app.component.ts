955831146e2c70ce2b15013855c89c26043a347c
Santi <santiago.pinto.100@gmail.com>
2023-07-25 20:32:51 -0500

    added link to discord server, the boards and mine counter now go vertical on small screens

--- gui/src/app/app.component.ts
+++ gui/src/app/app.component.ts
@@ -10,4 +10,9 @@ export class AppComponent {
 
     title = 'minesweeper';
     
+    constructor() {}
+
+    goToDiscord() {
+        window.open('https://discord.gg/hW2r7Ssuaa', '_blank');
+    }
 }
