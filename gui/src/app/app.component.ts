6b5618909ccdab915a102b8067707f70adc08c07
Santi <santiago.pinto.100@gmail.com>
2023-07-26 01:52:46 -0500

    header now disappears on small screens and the sidenav appears, components overall formatted better for smaller screens

--- gui/src/app/app.component.ts
+++ gui/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 
 @Component({
@@ -9,8 +9,18 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent {
 
     title = 'minesweeper';
+    windowSize;
     
     constructor() {}
+
+    ngOnInit() {
+        this.windowSize = window.screen.width;
+    }
+
+    @HostListener('window:resize', ['$event'])
+    onResize(event) {
+        this.windowSize = event.target.innerWidth;
+    }
 
     goToDiscord() {
         window.open('https://discord.gg/hW2r7Ssuaa', '_blank');
